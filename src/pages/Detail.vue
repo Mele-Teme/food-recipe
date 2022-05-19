ca3c5cd934e720b20658caf7caded4dae4924f30
Mele-Teme <melake0933@gmail.com>
2022-05-19 18:21:16 +0300

    initial

--- src/pages/Detail.vue
+++ src/pages/Detail.vue
@@ -16,7 +16,7 @@
             <img
               :src="displayImages[currentImageIndex]"
               alt=""
-              class="w-full h-80 object-cover object-center"
+              class="w-full h-80  object-center object-contain"
             />
             <div
               v-if="displayImages.length > 1"
