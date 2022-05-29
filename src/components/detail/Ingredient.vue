bdea148775083dc0c918bee459dc2b18b6bfa563
Mele-Teme <melake0933@gmail.com>
2022-05-29 15:29:48 +0300

    initial

--- src/components/detail/Ingredient.vue
+++ src/components/detail/Ingredient.vue
@@ -13,11 +13,9 @@
           :key="item"
           class="flex items-center ml-5 py-1 text-sm"
         > 
-        <div class="w-[10%]">
+        
           <svg class="w-6 h-6 fill-orange-700 MuiSvgIcon-root MuiSvgIcon-fontSizeMedium MuiSvgIcon-root MuiSvgIcon-fontSizeLarge css-1shn170" focusable="false" aria-hidden="true" viewBox="0 0 24 24" data-testid="CheckCircleOutlineIcon" tabindex="-1" title="CheckCircleOutline"><path d="M16.59 7.58 10 14.17l-3.59-3.58L5 12l5 5 8-8zM12 2C6.48 2 2 6.48 2 12s4.48 10 10 10 10-4.48 10-10S17.52 2 12 2zm0 18c-4.42 0-8-3.58-8-8s3.58-8 8-8 8 3.58 8 8-3.58 8-8 8z"></path></svg>
-          
-        </div>
-          <div class="flex w-[90%] ">
+          <div class="flex ">
             <p class="ml-1  break-all">
               {{ item}}
             </p>
